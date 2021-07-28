b7737a5d287482ec1f65002bfb948b61306fdf0e
Artem <a.khimin@yandex.ru>
2021-07-28 19:54:35 +0300

    Доделал задание

---
+++ Node.cs
@@ -0,0 +1,27 @@
+﻿namespace GB_02_05
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+
+        }
+
+        public Node() { }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        
+            
+    }
+}
